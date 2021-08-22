b9ac80d39396e5e8fe19c2875bdd628f38155c16
Francesco Face <francesco.face@gmail.com>
2021-08-22 22:25:56 +0200

    Skip transactions 'non contabilizzate' in Nexi transformer

--- tests/Transformer/NexiTest.php
+++ tests/Transformer/NexiTest.php
@@ -14,34 +14,25 @@ class NexiTest extends TestCase
 
         $transactions = $YNABTransactions;
 
-        $this->assertCount(3, $transactions->toArray());
+        $this->assertCount(2, $transactions->toArray());
 
         $transaction1 = [
-            'date' => '2021-08-13',
-            'payee' => 'GRUPPO SDA SRL',
-            'memo' => '',
-            'outflow' => '33.88',
-            'inflow' => '0.00'
-        ];
-        $this->assertEquals($transaction1, $transactions->getByIndex(0)->toArray());
-
-        $transaction2 = [
             'date' => '2021-08-03',
             'payee' => 'AMZ*Amazon.it',
             'memo' => '',
             'outflow' => '0.00',
             'inflow' => '53.25'
         ];
-        $this->assertEquals($transaction2, $transactions->getByIndex(1)->toArray());
+        $this->assertEquals($transaction1, $transactions->getByIndex(0)->toArray());
 
-        $transaction3 = [
+        $transaction2 = [
             'date' => '2021-08-03',
             'payee' => 'Subscription PRO',
             'memo' => '',
             'outflow' => '1.71',
             'inflow' => '0.00'
         ];
-        $this->assertEquals($transaction3, $transactions->getByIndex(2)->toArray());
+        $this->assertEquals($transaction2, $transactions->getByIndex(1)->toArray());
 
     }
 }
