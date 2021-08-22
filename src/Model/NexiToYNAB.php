7dc083aaecee10ae4ac41d4129620b1dbaf83e6c
Francesco Face <francesco.face@gmail.com>
2021-08-22 16:35:21 +0200

    Add transformer from Nexi to YNAB

---
+++ src/Model/NexiToYNAB.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Model;
+
+use Carbon\Carbon;
+use Model\Transaction\YNABTransaction;
+use Model\Transaction\YNABTransactions;
+use PhpOffice\PhpSpreadsheet\IOFactory;
+use PhpOffice\PhpSpreadsheet\Spreadsheet;
+
+class NexiToYNAB
+{
+    private const FIRST_DATA_ROW = 11;
+    private const COL_TO_CHECK_END = "B";
+
+    private Spreadsheet $nexiFile;
+
+    public function __construct($inputFilename)
+    {
+        $this->nexiFile = IOFactory::load($inputFilename);
+    }
+
+    public function process(): YNABTransactions
+    {
+        $YNABTransactions = new YNABTransactions();
+
+        $row = self::FIRST_DATA_ROW;
+        $end = false;
+        while (!$end) {
+            if ($this->nexiFile->getActiveSheet()->getCell(self::COL_TO_CHECK_END.$row)->getValue() == '') {
+                $end = true;
+            } else {
+                $date = Carbon::createFromFormat('d/m/Y', $this->nexiFile->getActiveSheet()->getCell('C'.$row)->getValue());
+                $payee = $this->nexiFile->getActiveSheet()->getCell('F'.$row)->getValue();
+                $memo = '';
+                $amount = $this->nexiFile->getActiveSheet()->getCell('J'.$row)->getValue();
+                $transaction = YNABTransaction::fromStrings(
+                    $date->format('Y-m-d'),
+                    $payee,
+                    $memo,
+                    $amount < 0 ? abs($amount) : 0,
+                    $amount > 0 ? abs($amount) : 0,
+                );
+                $YNABTransactions->add($transaction);
+                $row++;
+            }
+        }
+
+        return $YNABTransactions;
+    }
+}
---
+++ tests/Model/NexiToYNABTest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Tests\Model;
+
+use Model\NexiToYNAB;
+use PHPUnit\Framework\TestCase;
+
+class NexiToYNABTest extends TestCase
+{
+    public function test_process()
+    {
+        $nexiToYNAB = new NexiToYNAB(__DIR__.'/../Fixtures/movimenti-nexi.xlsx');
+        $YNABTransactions = $nexiToYNAB->process();
+
+        $transactionsArray = $YNABTransactions->toArray();
+
+        $this->assertCount(3, $transactionsArray);
+
+        $this->assertEquals('2021-08-13', $YNABTransactions->getByIndex(0)->getDate()->format('Y-m-d'));
+        $this->assertEquals('GRUPPO SDA SRL', $YNABTransactions->getByIndex(0)->getPayee());
+        $this->assertEquals('33.88', $YNABTransactions->getByIndex(0)->getInflowAsString());
+        $this->assertEquals('0.00', $YNABTransactions->getByIndex(0)->getOutflowAsString());
+
+        $this->assertEquals('2021-08-03', $YNABTransactions->getByIndex(1)->getDate()->format('Y-m-d'));
+        $this->assertEquals('AMZ*Amazon.it', $YNABTransactions->getByIndex(1)->getPayee());
+        $this->assertEquals('0.00', $YNABTransactions->getByIndex(1)->getInflowAsString());
+        $this->assertEquals('53.25', $YNABTransactions->getByIndex(1)->getOutflowAsString());
+
+        $this->assertEquals('2021-08-03', $YNABTransactions->getByIndex(2)->getDate()->format('Y-m-d'));
+        $this->assertEquals('Subscription PRO', $YNABTransactions->getByIndex(2)->getPayee());
+        $this->assertEquals('1.71', $YNABTransactions->getByIndex(2)->getInflowAsString());
+        $this->assertEquals('0.00', $YNABTransactions->getByIndex(2)->getOutflowAsString());
+    }
+}
