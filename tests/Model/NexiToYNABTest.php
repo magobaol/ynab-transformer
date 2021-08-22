b3213eccec1dc2432e9b7ba4883e53782687c8ce
Francesco Face <francesco.face@gmail.com>
2021-08-22 18:27:56 +0200

    Fix inflow and outflow logic for Nexi

--- tests/Model/NexiToYNABTest.php
+++ tests/Model/NexiToYNABTest.php
@@ -18,17 +18,17 @@ class NexiToYNABTest extends TestCase
 
         $this->assertEquals('2021-08-13', $YNABTransactions->getByIndex(0)->getDate()->format('Y-m-d'));
         $this->assertEquals('GRUPPO SDA SRL', $YNABTransactions->getByIndex(0)->getPayee());
-        $this->assertEquals('33.88', $YNABTransactions->getByIndex(0)->getInflowAsString());
-        $this->assertEquals('0.00', $YNABTransactions->getByIndex(0)->getOutflowAsString());
+        $this->assertEquals('0.00', $YNABTransactions->getByIndex(0)->getInflowAsString());
+        $this->assertEquals('33.88', $YNABTransactions->getByIndex(0)->getOutflowAsString());
 
         $this->assertEquals('2021-08-03', $YNABTransactions->getByIndex(1)->getDate()->format('Y-m-d'));
         $this->assertEquals('AMZ*Amazon.it', $YNABTransactions->getByIndex(1)->getPayee());
-        $this->assertEquals('0.00', $YNABTransactions->getByIndex(1)->getInflowAsString());
-        $this->assertEquals('53.25', $YNABTransactions->getByIndex(1)->getOutflowAsString());
+        $this->assertEquals('53.25', $YNABTransactions->getByIndex(1)->getInflowAsString());
+        $this->assertEquals('0.00', $YNABTransactions->getByIndex(1)->getOutflowAsString());
 
         $this->assertEquals('2021-08-03', $YNABTransactions->getByIndex(2)->getDate()->format('Y-m-d'));
         $this->assertEquals('Subscription PRO', $YNABTransactions->getByIndex(2)->getPayee());
-        $this->assertEquals('1.71', $YNABTransactions->getByIndex(2)->getInflowAsString());
-        $this->assertEquals('0.00', $YNABTransactions->getByIndex(2)->getOutflowAsString());
+        $this->assertEquals('0.00', $YNABTransactions->getByIndex(2)->getInflowAsString());
+        $this->assertEquals('1.71', $YNABTransactions->getByIndex(2)->getOutflowAsString());
     }
 }
