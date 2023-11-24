328076fa291af4d7f065ad7f0eb680681f8a0e7f
Francesco Face <francesco.face@gmail.com>
2023-11-24 23:07:59 +0100

    Add Revolut transformer

---
+++ src/Transformer/Revolut.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace Transformer;
+
+use Carbon\Carbon;
+use Model\Transaction\YNABTransaction;
+use Model\Transaction\YNABTransactions;
+use PhpOffice\PhpSpreadsheet\IOFactory;
+use PhpOffice\PhpSpreadsheet\Spreadsheet;
+use PhpOffice\PhpSpreadsheet\Style\NumberFormat;
+
+class Revolut implements Transformer
+{
+    private Spreadsheet $file;
+    private const FIRST_DATA_ROW = 2;
+    private const COL_TO_CHECK_END = "A";
+
+    public function __construct($inputFilename)
+    {
+        $this->file = IOFactory::load($inputFilename);
+    }
+
+    public function transformToYNAB(): YNABTransactions
+    {
+        $YNABTransactions = new YNABTransactions();
+        $row = self::FIRST_DATA_ROW;
+        while ($this->rowHasData($row)) {
+            if (!$this->shouldSkipRow($row)) {
+                $date = $this->getDate($row);
+                $payee = $this->getPayee($row);
+                $memo = '';
+                $amount = $this->getAmount($row);
+
+                $transaction = YNABTransaction::fromStrings(
+                    $date->format('Y-m-d'),
+                    $payee,
+                    $memo,
+                    $amount < 0 ? abs($amount) : 0,
+                    $amount > 0 ? abs($amount) : 0,
+                );
+                $YNABTransactions->add($transaction);
+            }
+            $row++;
+        }
+
+        return $YNABTransactions;
+    }
+
+    /**
+     * @param int $row
+     * @return bool
+     */
+    private function rowHasData(int $row): bool
+    {
+        return $this->file->getActiveSheet()->getCell(self::COL_TO_CHECK_END . $row)->getValue() != '';
+    }
+
+    /**
+     * @param int $row
+     * @return bool
+     */
+    private function shouldSkipRow(int $row): bool
+    {
+        return ($this->file->getActiveSheet()->getCell("I".$row)->getValue() != 'COMPLETED');
+    }
+
+    /**
+     * @param int $row
+     * @return Carbon|false
+     */
+    private function getDate(int $row): Carbon|false
+    {
+        $this->file->getActiveSheet()->getStyle('C' . $row)->getNumberFormat()->setFormatCode(NumberFormat::FORMAT_DATE_YYYYMMDD);
+        return Carbon::createFromFormat('Y-m-d H:i:s', $this->file->getActiveSheet()->getCell('C' . $row)->getFormattedValue());
+    }
+
+    private function getPayee(int $row)
+    {
+       return $this->file->getActiveSheet()->getCell("E".$row)->getValue();
+    }
+
+    private function getAmount(int $row)
+    {
+        return $this->file->getActiveSheet()->getCell('F' . $row)->getValue();
+    }
+}
---
+++ tests/Transformer/RevolutTest.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace Tests\Transformer;
+
+use PHPUnit\Framework\TestCase;
+use Transformer\Popso;
+use Transformer\Revolut;
+
+class RevolutTest extends TestCase
+{
+    public function test_should_skip_rows_with_state_not_completed()
+    {
+        $transformer = new Revolut(__DIR__.'/../Fixtures/movimenti-revolut-not-completed.csv');
+
+        $transactions = $transformer->transformToYNAB();
+
+        $this->assertCount(0, $transactions->toArray());
+    }
+
+    public function test_transform()
+    {
+        $transformer = new Revolut(__DIR__.'/../Fixtures/movimenti-revolut.csv');
+
+        $transactions = $transformer->transformToYNAB();
+
+        $this->assertCount(5, $transactions->toArray());
+
+        $transaction1 = [
+            'date' => '2023-10-01',
+            'payee' => 'Top-Up by *9999',
+            'memo' => '',
+            'outflow' => '0.00',
+            'inflow' => '40.00'
+        ];
+        $this->assertEquals($transaction1, $transactions->getByIndex(0)->toArray());
+
+        $transaction2 = [
+            'date' => '2023-10-01',
+            'payee' => 'To LISA SIMPSON',
+            'memo' => '',
+            'outflow' => '10.20',
+            'inflow' => '0.00'
+        ];
+        $this->assertEquals($transaction2, $transactions->getByIndex(1)->toArray());
+
+        $transaction3 = [
+            'date' => '2023-10-01',
+            'payee' => 'To BART SIMPSON',
+            'memo' => '',
+            'outflow' => '40.00',
+            'inflow' => '0.00'
+        ];
+        $this->assertEquals($transaction3, $transactions->getByIndex(2)->toArray());
+
+        $transaction4 = [
+            'date' => '2023-10-31',
+            'payee' => 'Top-Up by *9999',
+            'memo' => '',
+            'outflow' => '0.00',
+            'inflow' => '20.00'
+        ];
+        $this->assertEquals($transaction4, $transactions->getByIndex(3)->toArray());
+
+        $transaction5 = [
+            'date' => '2023-10-31',
+            'payee' => 'From MARGE SIMPSON',
+            'memo' => '',
+            'outflow' => '0.00',
+            'inflow' => '20.00'
+        ];
+        $this->assertEquals($transaction5, $transactions->getByIndex(4)->toArray());
+
+    }
+
+    public function test_canHandle_returns_true_for_valid_revolut_file()
+    {
+        $canHandle = Revolut::canHandle(__DIR__ . '/../Fixtures/movimenti-revolut.csv');
+        $this->assertTrue($canHandle);
+    }
+
+    public function test_canHandle_returns_true_for_valid_revolut_not_completed_file()
+    {
+        $canHandle = Revolut::canHandle(__DIR__ . '/../Fixtures/movimenti-revolut-not-completed.csv');
+        $this->assertTrue($canHandle);
+    }
+
+    public function test_canHandle_returns_false_for_non_revolut_file()
+    {
+        $canHandle = Revolut::canHandle(__DIR__ . '/../Fixtures/movimenti-fineco.xlsx');
+        $this->assertFalse($canHandle);
+    }
+
+    public function test_canHandle_returns_false_for_excel_file()
+    {
+        $canHandle = Revolut::canHandle(__DIR__ . '/../Fixtures/movimenti-fineco.xlsx');
+        $this->assertFalse($canHandle);
+    }
+
+    public function test_canHandle_returns_false_for_nonexistent_file()
+    {
+        $canHandle = Revolut::canHandle(__DIR__ . '/../Fixtures/nonexistent.csv');
+        $this->assertFalse($canHandle);
+    }
+
+}
