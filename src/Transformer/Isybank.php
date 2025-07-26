6cec286c1a1c792783f646d9eb6fc1172a949ffe
Francesco Face <francesco.face@gmail.com>
2025-07-26 16:30:38 +0200

    Add Isybank transformer

---
+++ src/Transformer/Isybank.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace Transformer;
+
+use Carbon\Carbon;
+use Model\Transaction\YNABTransaction;
+use Model\Transaction\YNABTransactions;
+use PhpOffice\PhpSpreadsheet\IOFactory;
+use PhpOffice\PhpSpreadsheet\Spreadsheet;
+use PhpOffice\PhpSpreadsheet\Style\NumberFormat;
+
+class Isybank implements Transformer
+{
+    private const FIRST_DATA_ROW = 15;
+    private const COL_TO_CHECK_END = "A";
+
+    private Spreadsheet $file;
+
+    public function __construct($inputFilename)
+    {
+        $this->file = IOFactory::load($inputFilename);
+    }
+
+    public function transformToYNAB(): YNABTransactions
+    {
+        $YNABTransactions = new YNABTransactions();
+
+        $row = self::FIRST_DATA_ROW;
+        while ($this->rowHasData($row)) {
+            if (!$this->shouldSkipRow($row)) {
+                $date = $this->getDate($row);
+                $payee = $this->getPayee($row);
+                $memo = '';
+                $amount = $this->getAmount($row);
+
+                $transaction = YNABTransaction::fromStrings(
+                    $date->format('Y-m-d'),
+                    $payee,
+                    $memo,
+                    $amount < 0 ? abs($amount) : 0,
+                    $amount > 0 ? abs($amount) : 0,
+                );
+                $YNABTransactions->add($transaction);
+            }
+            $row++;
+        }
+
+        return $YNABTransactions;
+    }
+
+    /**
+     * @param int $row
+     * @return bool
+     */
+    public function rowHasData(int $row): bool
+    {
+        return $this->file->getActiveSheet()->getCell(self::COL_TO_CHECK_END . $row)->getValue() != '';
+    }
+
+    private function shouldSkipRow(int $row)
+    {
+        return ($this->file->getActiveSheet()->getCell('E' . $row)->getValue() == 'NO');
+    }
+
+    /**
+     * @param int $row
+     * @return Carbon|false
+     */
+    public function getDate(int $row): Carbon|false
+    {
+        $this->file->getActiveSheet()->getStyle('A' . $row)->getNumberFormat()->setFormatCode(NumberFormat::FORMAT_DATE_DDMMYYYY);
+        return Carbon::createFromFormat('d/m/Y', $this->file->getActiveSheet()->getCell('A' . $row)->getFormattedValue());
+    }
+
+    /**
+     * @param int $row
+     * @return mixed
+     */
+    public function getPayee(int $row): mixed
+    {
+        return $this->file->getActiveSheet()->getCell('B' . $row)->getValue();
+    }
+
+    /**
+     * @param int $row
+     * @return mixed
+     */
+    public function getAmount(int $row): mixed
+    {
+        return $this->file->getActiveSheet()->getCell('H' . $row)->getValue();
+    }
+}
---
+++ tests/Transformer/IsybankTest.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace Tests\Transformer;
+
+use PHPUnit\Framework\TestCase;
+use Transformer\Isybank;
+use Transformer\Poste;
+
+class IsybankTest extends TestCase
+{
+
+    public function test_transform()
+    {
+        $transformer = new Isybank(__DIR__.'/../Fixtures/movimenti-isybank.xlsx');
+        $transactions = $transformer->transformToYNAB();
+        $this->assertCount(4, $transactions->toArray());
+
+        $transaction1 = [
+            'date' => '2025-07-25',
+            'payee' => 'Addebito diretto disposto a favore di FASTWEB SPA MANDATO 3F9999A00000000',
+            'memo' => '',
+            'outflow' => '28.95',
+            'inflow' => '0.00'
+        ];
+        $this->assertEquals($transaction1, $transactions->getByIndex(0)->toArray());
+
+        $transaction2 = [
+            'date' => '2025-07-22',
+            'payee' => 'D\'ada Srl Via Prenestina 38',
+            'memo' => '',
+            'outflow' => '10.00',
+            'inflow' => '0.00'
+        ];
+        $this->assertEquals($transaction2, $transactions->getByIndex(1)->toArray());
+
+        $transaction3 = [
+            'date' => '2025-07-20',
+            'payee' => 'Le Delizie Di Zio Bibi Via',
+            'memo' => '',
+            'outflow' => '40.40',
+            'inflow' => '0.00'
+        ];
+        $this->assertEquals($transaction3, $transactions->getByIndex(2)->toArray());
+
+        $transaction4 = [
+            'date' => '2025-07-18',
+            'payee' => 'Ferretti Matteo Via Frascat',
+            'memo' => '',
+            'outflow' => '9.50',
+            'inflow' => '0.00'
+        ];
+        $this->assertEquals($transaction4, $transactions->getByIndex(3)->toArray());
+
+    }
+
+    public function test_transform_should_skip_rows_with_status_not_contabilizzato()
+    {
+        $transformer = new Isybank(__DIR__ . '/../Fixtures/movimenti-isybank-test-contabilizzati.xlsx');
+        $transactions = $transformer->transformToYNAB();
+        $this->assertCount(1, $transactions->toArray());
+
+        $transaction1 = [
+            'date' => '2025-07-24',
+            'payee' => 'Street Of Crocodiles',
+            'memo' => '',
+            'outflow' => '20.00',
+            'inflow' => '0.00'
+        ];
+        $this->assertEquals($transaction1, $transactions->getByIndex(0)->toArray());
+
+    }
+
+    public function test_canHandle_returns_true_for_valid_isybank_file()
+    {
+        $canHandle = Isybank::canHandle(__DIR__ . '/../Fixtures/movimenti-isybank.xlsx');
+        $this->assertTrue($canHandle);
+    }
+
+    public function test_canHandle_returns_true_for_valid_isybank_test_file()
+    {
+        $canHandle = Isybank::canHandle(__DIR__ . '/../Fixtures/movimenti-isybank-test-contabilizzati.xlsx');
+        $this->assertTrue($canHandle);
+    }
+
+    public function test_canHandle_returns_false_for_non_isybank_file()
+    {
+        $canHandle = Isybank::canHandle(__DIR__ . '/../Fixtures/movimenti-fineco.xlsx');
+        $this->assertFalse($canHandle);
+    }
+
+    public function test_canHandle_returns_false_for_csv_file()
+    {
+        $canHandle = Isybank::canHandle(__DIR__ . '/../Fixtures/movimenti-revolut.csv');
+        $this->assertFalse($canHandle);
+    }
+
+    public function test_canHandle_returns_false_for_nonexistent_file()
+    {
+        $canHandle = Isybank::canHandle(__DIR__ . '/../Fixtures/nonexistent.xlsx');
+        $this->assertFalse($canHandle);
+    }
+
+}
