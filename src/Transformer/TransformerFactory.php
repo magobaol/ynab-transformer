af3c0af6540ee348f5432d4032f7fa7dc078b4d9
Francesco Face <francesco.face@gmail.com>
2025-09-19 00:31:21 +0200

    Add service layer and update TransformerFactory with automatic format detection

--- src/Transformer/TransformerFactory.php
+++ src/Transformer/TransformerFactory.php
@@ -2,11 +2,21 @@
 
 namespace Transformer;
 
-use Exception;
+use Transformer\Fineco;
+use Transformer\Isybank;
+use Transformer\Nexi;
+use Transformer\Popso;
+use Transformer\Poste;
+use Transformer\Revolut;
+use Transformer\Telepass;
+use Transformer\Transformer;
 
 class TransformerFactory
 {
-    private static array $transformers = [
+    /**
+     * List of available transformers in order of popularity/usage
+     */
+    private const TRANSFORMERS = [
         'fineco' => Fineco::class,
         'revolut' => Revolut::class,
         'nexi' => Nexi::class,
@@ -17,57 +27,62 @@ class TransformerFactory
     ];
 
     /**
-     * Detect the format of the given file by checking each transformer's canHandle method
-     * 
-     * @param string $filename
+     * Detect which transformer can handle the given file
+     *
+     * @param string $filename Path to the file to analyze
      * @return string The detected format name
-     * @throws Exception if no format is detected or multiple formats match
+     * @throws \Exception If no format is detected or multiple formats match
      */
     public static function detectFormat(string $filename): string
     {
         $detectedFormats = [];
-        
-        foreach (self::$transformers as $formatName => $transformerClass) {
+
+        foreach (self::TRANSFORMERS as $format => $transformerClass) {
             if ($transformerClass::canHandle($filename)) {
-                $detectedFormats[] = $formatName;
+                $detectedFormats[] = $format;
             }
         }
-        
+
         if (empty($detectedFormats)) {
-            throw new Exception('No supported format detected for file: ' . basename($filename));
+            $supportedFormats = implode(', ', array_keys(self::TRANSFORMERS));
+            throw new \Exception("No supported format detected. Supported formats: {$supportedFormats}");
         }
-        
+
         if (count($detectedFormats) > 1) {
-            // Log all matched formats for debugging
-            error_log('Multiple formats detected for file ' . basename($filename) . ': ' . implode(', ', $detectedFormats));
-            throw new Exception('Multiple formats detected for file: ' . basename($filename));
+            $detectedFormatsList = implode(', ', $detectedFormats);
+            throw new \Exception("Multiple formats detected ({$detectedFormatsList}). Please specify manually with --format=<format>");
         }
-        
+
         return $detectedFormats[0];
     }
 
     /**
-     * Create a transformer instance for the given file
-     * 
-     * @param string $filename
-     * @return Transformer
-     * @throws Exception if no format is detected
+     * Create a transformer instance for the specified format
+     *
+     * @param string $format The format name
+     * @param string $filename Path to the file to transform
+     * @return Transformer The transformer instance
+     * @throws \Exception If the format is not supported
      */
-    public static function create(string $filename): Transformer
+    public static function create(string $format, string $filename): Transformer
     {
-        $format = self::detectFormat($filename);
-        $transformerClass = self::$transformers[$format];
-        
+        if (!isset(self::TRANSFORMERS[$format])) {
+            $supportedFormats = implode(', ', array_keys(self::TRANSFORMERS));
+            throw new \Exception("Unsupported format '{$format}'. Supported formats: {$supportedFormats}");
+        }
+
+        $transformerClass = self::TRANSFORMERS[$format];
         return new $transformerClass($filename);
     }
 
     /**
-     * Get a list of all supported format names
-     * 
-     * @return array
+     * Get list of all supported formats
+     *
+     * @return array List of supported format names
      */
     public static function getSupportedFormats(): array
     {
-        return array_keys(self::$transformers);
+        return array_keys(self::TRANSFORMERS);
     }
 }
+
